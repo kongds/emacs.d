2a35eb1187f5c73b827e8e15107de31b63d554ce
kongds <j614023177@icloud.com>
2017-06-23 21:14:39 +0800

    change file path

--- lisp/init-cc.el
+++ lisp/init-cc.el
@@ -20,7 +20,36 @@
 (define-key c++-mode-map (kbd "s-r") '(lambda () (interactive) (compile-cc t)))
 (define-key c-mode-map   (kbd "s-r") '(lambda () (interactive) (compile-cc nil)))
 
-(provide 'init-cc)
+;;;;;;;;;;;;;;;;;;;;;;
+;;irony-init
+;;;;;;;;;;;;;;;;;;;;;;
+;;hooks
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
 
-(shell-command "ls | grep make")
-(if  (eq (length (shell-command-to-string "ls | grep Make")) 0))
+;;company
+(eval-after-load 'company
+  '(add-to-list 'company-backends 'company-irony))
+(require 'company-irony-c-headers)
+;; Load with `irony-mode` as a grouped backend
+(eval-after-load 'company
+  '(add-to-list
+    'company-backends '(company-irony-c-headers company-irony)))
+
+;;flycheck
+(eval-after-load 'flycheck
+  '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
+
+
+;; replace the `completion-at-point' and `complete-symbol' bindings in
+;; irony-mode's buffers by irony-mode's function
+(defun my-irony-mode-hook ()
+  (define-key irony-mode-map [remap completion-at-point]
+    'irony-completion-at-point-async)
+  (define-key irony-mode-map [remap complete-symbol]
+    'irony-completion-at-point-async))
+(add-hook 'irony-mode-hook 'my-irony-mode-hook)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+
+(provide 'init-cc)
